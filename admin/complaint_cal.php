748b0c142e84128be7aea42640ce861140459fd9
ALYafei516 <abdulrahman.bin.gh@gmail.com>
2023-07-02 21:00:30 +0800

    Create complaint_cal.php

---
+++ admin/complaint_cal.php
@@ -0,0 +1,147 @@
+                <?php
+
+                include('../class/Appointment.php');
+
+				$object = new Appointment;
+
+				if(!$object->is_login())
+				{
+				    header("location:".$object->base_url."");
+				}
+
+                if($_SESSION['type'] != 'Admin')
+                {
+                    header("location:".$object->base_url."");
+                }
+
+                include('header.php');
+
+                ?>
+
+                     <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Complaints Calculations</h1>
+
+                    <!-- Content Row -->
+                    <div class="row row-cols-5">
+                        
+                        <div class="col mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                Today Total Complaints</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $object->get_total_today_complaints(); ?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Earnings (Monthly) Card Example -->
+                        <div class="col mb-4">
+                            <div class="card border-left-secondary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-secondary text-uppercase mb-1">
+                                                Yesterday Total Complaints</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $object->get_total_yesterday_complaints(); ?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col mb-4">
+                            <div class="card border-left-warning shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                                Last 7 Days Total Complaints</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $object->get_total_seven_day_complaints(); ?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col mb-4">
+                            <div class="card border-left-success shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                Total Complaints till date</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $object->get_total_complaints(); ?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col mb-4">
+                            <div class="card border-left-danger shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                                Total Unsatisfied Expert’s Feedback Complaints</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $object->get_total_today_usf(); ?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                   
+					 <div class="col mb-4">
+                            <div class="card border-left-danger shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                                Total Wrongly Assigned Research Area Complaints</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $object->get_total_today_wara(); ?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                   
+					
+					 <div class="col mb-4">
+                            <div class="card border-left-danger shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                                Total Other Type Complaints</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $object->get_total_today_other(); ?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php
+                include('footer.php');
+                ?>
